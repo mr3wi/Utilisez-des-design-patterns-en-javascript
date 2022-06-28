cd569331d05093968f7d6f3933c7253482047154
mr3wi <wilfried.wiwi@epitech.eu>
2022-06-28 17:00:53 +0100

    Proxy Pattern

--- js/proxy/ProxyRatingSorter.js
+++ js/proxy/ProxyRatingSorter.js
@@ -3,6 +3,17 @@ class ProxyRatingSorter {
         this.cache = []
     }
 
-    async sorter() {
+    async sorter(movies, orderBy) {
+        const  cacheResult = this.cache.find(elt => elt.key === orderBy)
+        
+        if (cacheResult) {
+            console.log('get from cache')
+            return cacheResult
+        }
+
+        const data = await RatingSorterApi.sorter(movies, orderBy)
+
+        this.cache.push(data)
+        return data
     }
 }
