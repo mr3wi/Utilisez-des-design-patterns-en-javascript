ba156e1e47a44b37a6fa6a4724fc437db0e3913f
mr3wi <wilfried.wiwi@epitech.eu>
2022-06-29 18:26:44 +0100

    Template Pattern search

--- js/search/MainSearch.js
+++ js/search/MainSearch.js
@@ -1,7 +1,31 @@
-class Search {}
+class Search {
+    constructor(movie) {
+        this.movie = movie
+    }
+
+    search(query) {
+        return this.filterMovie(query)
+    }
+}
 
 
-class MovieNameSearch extends Search {}
+class MovieNameSearch extends Search {
+    constructor(movie) {
+        super(movie)
+    }
+
+    filterMovie(query) {
+        return this.movie.filter(movie => movie.title.toLowerCase().includes(query.toLowerCase()))
+    }
+}
 
 
-class ActorNameSearch extends Search {}
+class ActorNameSearch extends Search {
+    constructor(movie) {
+        super(movie)
+    }
+
+    filterMovie(query) {
+        return this.movie.filter(movie => movie.actor.toLowerCase().includes(query.toLowerCase()))
+    }
+}
